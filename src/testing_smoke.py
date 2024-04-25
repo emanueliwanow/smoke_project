eda14551103c240068ba80c11668a8202e57d907
WiMi Administrator <emanueldearaujo123@gmail.com>
2024-04-25 09:35:51 +0200

    More corrections 3

--- src/testing_smoke.py
+++ src/testing_smoke.py
@@ -9,16 +9,6 @@ class SMOKE:
         self.smoke = rospy.ServiceProxy("mavros/cmd/command", CommandLong)
     def actv_smoke(self,open):
         command = CommandLong()
-        command.broadcast = False
-        command.confirmation = 0
-        command.command = 187
-        command.param1 = open
-        command.param2 = 0
-        command.param3 = 0
-        command.param4 = 0
-        command.param5 = 0
-        command.param6 = 0
-        command.param7 = 0
         self.smoke.call(False,187,0,open,0,0,0,0,0,0)
 
 
