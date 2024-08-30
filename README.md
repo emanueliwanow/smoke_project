0c4cbd2abbdad766bdd220a695be05670d1970ab
emanueliwanowTUD <emanuel.iwanow@stud.tu-darmstadt.de>
2024-08-30 13:06:01 +0200

    Added functions

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # smoke_project
 Repository for implementing MPC control to stabilize a MAV.
+
+
+## Installing the PX4 Simulation 
+
+```
+sudo apt update
+sudo apt upgrade
+sudo apt install git
+mkdir src
+cd src
+git clone https://github.com/PX4/Firmware.git --recursive
+cd Firmware
+bash ./Tools/setup/ubuntu.sh
+
+## reboot computer
+https://raw.githubusercontent.com/ktelegenov/scripts/main/ubuntu_sim_ros_noetic.sh
+bash ubuntu_sim_ros_noetic.sh
+
+## close the terminal and open it again
+cd src/Firmware
+git submodule update --init --recursive
+DONT_RUN=1 make px4_sitl_default gazebo
+
+source Tools/simulation/gazebo/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd):$(pwd)/Tools/simulation/gazebo/sitl_gazebo
+
+## Make sure to add the above inside the .bashrc file if you want to run it everytime from the terminal. The $pwd should be replaced with the path to Firmware folder.
+
+roslaunch px4 multi_uav_mavros_sitl.launch
+```
---
+++ src/simple_circle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+import rospy
+from mavbase.MAV import MAV
+import numpy as np
+import math
+
+def go():
+
+    rospy.init_node("mav_test")
+    mav = MAV("1")
+
+    altitude = 0.9
+    takeoff_alt = altitude
+    start_x = 0
+    radius = 1.5
+    start_y = radius
+    start_yaw = -math.pi/2
+    circle_discretization = 20
+    time_step = 100
+    angle_step = 2*math.pi/circle_discretization
+    center_circle_x = 0
+    center_circle_y = 0
+    
+  
+    
+    mav.takeoff_and_keep_yaw(takeoff_alt)
+    mav.hold(4)
+    rospy.loginfo("Takeoff finished")
+    
+    mav.set_position_with_yaw(start_x,start_y,altitude,start_yaw)   
+    mav.hold(2) 
+    
+   
+    
+    rospy.loginfo("Starting circle")
+      
+    
+    for step in range(circle_discretization+1):
+    	
+        angle = -start_yaw+ (step*angle_step)
+        goal_x = (center_circle_x-math.cos(angle))*radius
+        goal_y = (center_circle_y+math.sin(angle))*radius
+        rospy.loginfo("Updating position")
+        goal_yaw = -angle
+        mav.set_position_with_yaw(goal_x,goal_y,altitude,goal_yaw)
+        
+    
+   
+    rospy.loginfo("Circle finished")
+    """
+    
+    for j in range(circle_discretization*time_step):
+    	k = j//time_step
+    	start_yaw = angle_step * k
+    	mav.set_position_with_yaw(start_x, start_y, altitude, start_yaw)
+
+    """
+    
+    
+    mav.land()
+    mav._disarm()
+    
+if __name__ == "__main__":
+    go()
