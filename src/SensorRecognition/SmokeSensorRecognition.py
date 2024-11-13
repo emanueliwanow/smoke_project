d693fd751cbadb8cee9595fed718bafdc5590c6f
emanueliwanowTUD <emanuel.iwanow@stud.tu-darmstadt.de>
2024-11-13 16:29:50 +0100

    Fix

--- src/SensorRecognition/SmokeSensorRecognition.py
+++ src/SensorRecognition/SmokeSensorRecognition.py
@@ -86,7 +86,8 @@ class ImageListener:
 
     def estimate_sensor_position(self, depth_image,u,v):
         cv_depth_image = self.bridge.imgmsg_to_cv2(depth_image,depth_image.encoding)
-        dist = cv_depth_image[u,v]
+        rospy.loginfo(f'Depth image size {cv_depth_image.shape}')
+        dist = cv_depth_image[v,u]
         Xtarget = dist*(u-self.cx)/(self.fx)
         Ytarget = dist*(v-self.cy)/(self.fy)
         Ztarget = dist
