838488de9a1b6db64ba371dd29bc6e44ea15f79c
emanueliwanowTUD <emanuel.iwanow@stud.tu-darmstadt.de>
2024-11-20 11:55:57 +0100

    Disabled depth

--- src/SensorRecognition/SmokeSensorRecognition.py
+++ src/SensorRecognition/SmokeSensorRecognition.py
@@ -39,13 +39,17 @@ class ImageListener:
         self.cx = self.camera_info.K[2]
         self.cy = self.camera_info.K[5]
 
-        self.depth_topic = '/d435/depth/image_rect_raw'
-        self.depth_data = Image()
+        # Depth was turned off because it influences the mocap system
+        #self.depth_topic = '/d435/depth/image_rect_raw'
+        #self.depth_data = Image()
+
+        # Instead of the depth information, the altitude of the smoke detector will be the input parameter
+        self.smoke_sensor_altitude = 2.8 
 
 
         self.bridge = CvBridge()
         self.rgb_sub = rospy.Subscriber(self.rgb_topic,Image,self.imageCallback)
-        self.depth_sub = rospy.Subscriber(self.depth_topic,Image,self.depthCallback)
+        #self.depth_sub = rospy.Subscriber(self.depth_topic,Image,self.depthCallback)
 
 
         
@@ -66,8 +70,8 @@ class ImageListener:
     def imageCallback(self,data):
         self.rgb_data = data
 
-    def depthCallback(self,data):
-        self.depth_data = data
+    #def depthCallback(self,data):
+    #    self.depth_data = data
 
     def dronePoseCallback(self,data):
         self.drone_pose = data
@@ -93,17 +97,20 @@ class ImageListener:
         
         return int(x_min), int(y_min), int(x_max), int(y_max), x_center,y_center
 
-    def estimate_sensor_position(self, depth_image,u,v):
-        cv_depth_image = self.bridge.imgmsg_to_cv2(depth_image,depth_image.encoding)
-        
-        dist = cv_depth_image[v,u]/1000 # Pass to meters
+    def estimate_sensor_position(self,u,v,drone_x,drone_y,drone_z):
+        #cv_depth_image = self.bridge.imgmsg_to_cv2(depth_image,depth_image.encoding)        
+        #dist = cv_depth_image[v,u]/1000 # Pass to meters
+
+        dist = self.smoke_sensor_altitude - drone_z
+
+
         Ytarget = -dist*(u-self.cx)/(self.fx) # Putting the coordinates the as the drone X Front, Y positive right
         Xtarget = dist*(v-self.cy)/(self.fy)
         Ztarget = dist
         if self.pose_estimation_type == 'global':
-            Ytarget = Ytarget + self.drone_pose.pose.position.y
-            Xtarget = Xtarget + self.drone_pose.pose.position.x
-            Ztarget = Ztarget + self.drone_pose.pose.position.z
+            Ytarget = Ytarget + drone_y
+            Xtarget = Xtarget + drone_x
+            Ztarget = Ztarget + drone_z
 
         return Xtarget,Ytarget,Ztarget
 
@@ -115,7 +122,11 @@ class ImageListener:
         try:
             
             rgb_data = self.rgb_data
-            depth_data = self.depth_data
+            #depth_data = self.depth_data
+            drone_x = self.drone_pose.pose.position.x
+            drone_y = self.drone_pose.pose.position.y
+            drone_z = self.drone_pose.pose.position.z
+            
 
             cv_rgb_image = self.bridge.imgmsg_to_cv2(rgb_data, rgb_data.encoding)
             
@@ -130,7 +141,7 @@ class ImageListener:
 
                 x_min, y_min, x_max, y_max, x_center, y_center = self.get_center_of_box(bounding_boxes)
                 self.pub_bb_image.publish(self.bridge.cv2_to_imgmsg(result_img, encoding='rgb8'))  ###Bei Jetson Verion herausnehmen (nur zu visualisierungszwecken)
-                Xtarget,Ytarget,Ztarget = self.estimate_sensor_position(depth_data,x_center,y_center)
+                Xtarget,Ytarget,Ztarget = self.estimate_sensor_position(x_center,y_center,drone_x,drone_y,drone_z)
                 array_msg.data = [Xtarget,Ytarget,Ztarget] 
                 #rospy.loginfo(f'Object detected X:{Xtarget}, Y:{Ytarget}, Z:{Ztarget}')
                 #array_msg.data = [x_min, y_min, x_max, y_max, x_center, y_center,self.rbg_data.height, self.rbg_data.width]  # Example array values 
